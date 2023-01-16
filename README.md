406d832397a31de2af2405b53163788c7c453155
Daniel Lawrence <dannyla@linux.com>
2023-01-16 14:53:11 +1100

    Speed up execution on massive mbox files

--- README.md
+++ README.md
@@ -1,46 +1,7 @@
 # Google Takeout Helper
 
-## Install
-1. Make sure Python 3 is installed as well as `pip` for python 3
-1. Install ImageMagick version 7 with HEIC support.
-  * uninstall default libhefi (if it's older than 1.51.  If it's >= 1.5.1 you can skip building and installing libheif)
-  * build and install (libheif-1.5.1)[https://github.com/ImageMagick/libheif] manually from source. This was required for
-    me on Ubuntu to build ImageMagick 7 correctly with HEIC support.
-  * `sudo apt install build-essentials libheif-dev`
-  * Download source for ImageMagick 7
-  * Uncomment all 'build-deps's in `/etc/apt/sources.list`
-  * `apt-get build-dep imagemagick`
-  * `./configure --with-heic`
-  * `make`
-  * `sudo make install`
-  * `sudo ldconfig /usr/local/lib`
-1. Install `libmagickwand-dev`?
-1. Install Wand: `pip install Wand`
+Extract all attachments from an mbox file 
 
-## Recover your photos from Google Photos
-
-1. Download all `.zip` archives for
-   [*only* Google Photos data](https://takeout.google.com/settings/takeout).
-   For me, selecting more than Google Photos resulted in an error. This is most
-   likely because of the size of **all** of my Google data being so massive.
-    *  Note: It can take hours or even days for Google to generate Zip files with
-       all your data.
-1. Run `./organize.py <directory where all your archives are>`.  For example:
-   `./organize.py --photos_dir ~/Downloads/google_takeout_archives/`.  The
-   script will:
-    *  Find all of the takeout archives in this directory
-    *  Extract the photos from these archives
-    *  Delete extra metadata files (i.e. not the images and videos)
-    *  Give you an option to delete the archives after to reclaim some hard
-       drive space.
-1. (Strongly recommended) Back up your photos somewhere else.  Google does a ton
-   of work to make sure your images will never be accidentally deleted or lost.
-   It's very unlikely you are willing/able/can afford to have the same
-   level of reliability as Google.  I'm no expert, but I've found [this subreddit's
-   wiki](https://www.reddit.com/r/DataHoarder/wiki/backups) to be a good
-   starting point for learning about how to back up.
-
-## Extract all attachments from the mail archive
-
-1.  Download the `.mbox` archive with Gmail data
-2.  Run `./organize.py --mbox_file <path to .mbox file>`
+```
+./main.py path/to/your.mbox
+```
---
+++ main.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+"""
+Module for extracting all attachments from an mbox archive.
+
+rework of https://github.com/treymo/google-takeout-helper
+"""
+    
+import sys
+import mailbox
+import os
+import re
+import time
+import datetime
+import email
+from email.policy import default
+
+
+class MboxReader:
+    """ lifted from https://stackoverflow.com/questions/59681461/read-a-big-mbox-file-with-python. """
+    def __init__(self, filename):
+        self.handle = open(filename, 'rb')
+        assert self.handle.readline().startswith(b'From ')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.handle.close()
+
+    def __iter__(self):
+        return iter(self.__next__())
+
+    def __next__(self):
+        lines = []
+        while True:
+            line = self.handle.readline()
+            if line == b'' or line.startswith(b'From '):
+                yield email.message_from_bytes(b''.join(lines), policy=default)
+                if line == b'':
+                    break
+                lines = []
+                continue
+            lines.append(line)
+
+class Message:
+    def __init__(self, message):
+        self.message = message
+    
+    @property
+    def subject(self):
+        return self.message.get('subject', '')
+    
+    @property
+    def is_spam(self):
+        return 'Spam' in self.message.get('X-Gmail-Labels', '')
+    
+    @property
+    def has_attachment(self):
+        return self.message.is_multipart()
+
+    @property
+    def sender(self):
+        _sender = self.message.get('From', '')
+        return _sender.split('<')[-1].split('>')[0]
+
+    @property
+    def sent(self):
+        try:
+            date_string = self.message['Date']
+            sent = datetime.datetime.strptime(date_string, '%a, %d %b %Y %H:%M:%S %z')
+            return sent.strftime('%Y-%b')
+        except Exception:
+            pass
+
+    def iter_attachments(self):
+        return self.message.iter_attachments()
+
+    def __str__(self):
+        return f'{self.sent} {self.sender:<100} {self.subject}'
+
+
+def strip_illegal_char(input_string):
+    return re.sub(r'[\<,\>,\:,\",\/,\\,\|,\?,\*,\n,\t]', '', str(input_string))
+
+
+def extract_mail_attachments(mbox_file_path):
+    with MboxReader(mbox_file_path) as mbox:
+        for message in mbox:
+            message = Message(message)
+
+            if message.is_spam:
+                continue
+
+            if not message.has_attachment:
+                continue
+                
+            if not message.sender or not message.subject or not message.sent:
+                continue
+
+            print(message)
+
+            for attachment in message.iter_attachments():
+                filename = attachment.get_filename()
+                if not filename:
+                    continue
+                filename = strip_illegal_char(filename)
+                d = os.path.join(f'{mbox_file_path}_attachments', message.sent)
+                p = os.path.join(d, f'{message.sent}--{message.sender}--{filename}')
+                os.makedirs(d, exist_ok=True)
+                with open(p, 'wb') as a:
+                    try:
+                        a.write(attachment.get_payload(decode=True))
+                    except Exception as e:
+                        continue
+                print(f'    {p}')
+
+if __name__ == '__main__':
+    extract_mail_attachments(sys.argv[1])
